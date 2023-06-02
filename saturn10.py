b445267ccd48ae48e2943b187b950e308ccd37d8
Godwin Akpa <135206747+Godwin-Akpa@users.noreply.github.com>
2023-06-02 21:45:32 +0100

    Update saturn10.py

---
+++ saturn10.py
@@ -0,0 +1,8 @@
+imtran teli is very good in DevOps
+i really want to master it so much
+that is why i am learning right now
+aslo venture into ALX SE program
+which is going to end around July the following year
+i have gotten my first transcript 
+i was so happy about it though
+So determined to achieve this!
